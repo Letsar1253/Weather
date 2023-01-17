2c5b2eacece3a3507546b3ad7474a114d80dd01a
shit, it's not google <k1merpro@gmail.com>
2023-01-17 22:34:42 +0700

    Наработки

--- Weather/Weather/MainPage.xaml.cs
+++ Weather/Weather/MainPage.xaml.cs
@@ -1,24 +1,82 @@
 ﻿namespace Weather;
 
+enum WeatherType {
+    Sunny,
+    Rain,
+    Haze,
+    Cloudly,
+    Overcast_Cloudly,
+    Sleet,
+    Snow,
+    Thunder,
+    Thunder_Rain
+};
+
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    readonly Dictionary<WeatherType, string> weatherTypeImages = new() {
+        { WeatherType.Sunny,            "sunny" },
+        { WeatherType.Rain,             "rain" },
+        { WeatherType.Haze,             "haze" },
+        { WeatherType.Cloudly,          "cloudly" },
+        { WeatherType.Overcast_Cloudly, "overcast_cloudly" },
+        { WeatherType.Sleet,            "sleet" },
+        { WeatherType.Snow,             "snow" },
+        { WeatherType.Thunder,          "thunder" },
+        { WeatherType.Thunder_Rain,     "thunder_rain" }
+    };
 
-	public MainPage()
-	{
-		InitializeComponent();
+	readonly Color ColdColor = Color.FromRgb( 212, 244, 255 );
+	readonly Color WarmColor = Color.FromRgb( 255, 236, 225 );
+	readonly double ColdBorder = -40, WarmBorder = 50;
+
+	private void setDayData( DateTime date ) {
+
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+    private void setTemperature( double temperature ) { 
+        MainPageObject.BackgroundColor = getGradientColor(
+            ( temperature - ColdBorder ) / ( WarmBorder - ColdBorder ) );
+
+
+        // Настроить градусник
+    }
+
+    private void setWeatherImage( WeatherType weatherType ) {
+        Image weatherImage = new() {
+            Source = weatherTypeImages[ weatherType ] + ".png",
+            HeightRequest = 30,
+            HorizontalOptions = LayoutOptions.Center
+        };
+        MainPageLayout.Children.Add( weatherImage );
+    }
+
+    private Color getGradientColor( double value ) {
+		if( value > 1 ) return WarmColor;
+        if( value < 0 ) return ColdColor;
+
+		double r = WarmColor.Red * value + ColdColor.Red * ( 1 - value );
+        double g = WarmColor.Green * value + ColdColor.Green * ( 1 - value );
+        double b = WarmColor.Blue * value + ColdColor.Blue * ( 1 - value );
+
+        return Color.FromRgb( r, g, b );
+	}
+
+    public MainPage()
 	{
-		count++;
+        InitializeComponent();
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        // WheaterData Какая погода, градусы, дата
+        WeatherType weatherType = WeatherType.Sunny;
+		double temperature = 31.5;
+		DateTime date = new();
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		setDayData( date );
+		setTemperature( temperature );
+		setWeatherImage( weatherType );
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e) {
 	}
 }
 
--- Weather/Weather/Resources/NewPage1.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Weather.Resources;
-
-public partial class NewPage1 : ContentPage
-{
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
-}
