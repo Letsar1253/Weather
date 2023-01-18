e696c80898dd0a1c5bbee651c1626cc28b7ce100
SroMan-io <slivkin.roma@yandex.ru>
2023-01-18 19:34:47 +0700

    db

---
+++ Weather/Server.DatabaseProvider/Models/HourlyWeather.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.DatabaseProvider.Models
+{
+    internal class HourlyWeather : Common.Data.Models.HourlyWeather
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid WeatherIconId { get; set; }
+
+        public WeatherIcon weatherIcon { get; set; }
+    }
+}
